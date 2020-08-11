a336a13dfa7bda9984079a00549474807a9a7031
Nick <nletdara@gmail.com>
2020-08-11 19:46:31 -0400

    removed booking components and modules

--- sched-book/src/app/app.module.ts
+++ sched-book/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { BookingModule } from './modules/booking/booking.module';
-
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +14,6 @@ import { BookingModule } from './modules/booking/booking.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    BookingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
