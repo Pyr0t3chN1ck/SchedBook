569fc10229aa119ea61335b8db38fcbbd17d4533
Nick <nletdara@gmail.com>
2020-09-03 23:06:59 -0400

    added form group controls to contact form

--- sched-book/src/app/modules/address-book/contact-form/contact-form.component.ts
+++ sched-book/src/app/modules/address-book/contact-form/contact-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { PhoneNumber } from '../phone-number-input/phone-number-input.component';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,16 +9,31 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  contactForm = this.formBuilder.group({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl(''),
+    address: new FormControl(''),
+    phoneNumber: new FormControl(new PhoneNumber('', '', '')),
+    email: new FormControl('', [Validators.email]),
+    dateOfBirth: new FormControl(new Date()),
+    preferredBrands: new FormControl(''),
+    preferredColors: new FormControl(''),
+  });
 
-  constructor(private dialogRef: MatDialogRef<ContactFormComponent>) {
+  constructor(private dialogRef: MatDialogRef<ContactFormComponent>, private formBuilder: FormBuilder) {
     dialogRef.disableClose = true;
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  onCancelClick(): void {
+  onCancel(): void {
     this.dialogRef.close();
   }
 
+  onSave() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
 }
