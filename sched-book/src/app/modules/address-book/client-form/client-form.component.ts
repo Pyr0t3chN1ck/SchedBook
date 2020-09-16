ed515f010eeeb1a4cd3dab720353916bb7de6190
Nick <nletdara@gmail.com>
2020-09-16 17:29:10 -0400

    add client form now has edit capabilites and can now edit clients

--- sched-book/src/app/modules/address-book/client-form/client-form.component.ts
+++ sched-book/src/app/modules/address-book/client-form/client-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, EventEmitter, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { PhoneNumber } from '../phone-number-input/phone-number-input.component';
 import { Client } from 'src/app/shared/models';
@@ -22,11 +22,30 @@ export class ClientFormComponent implements OnInit {
   });
   save = new EventEmitter<Client>();
 
-  constructor(private dialogRef: MatDialogRef<ClientFormComponent>, private formBuilder: FormBuilder) {
+  constructor(private dialogRef: MatDialogRef<ClientFormComponent>, private formBuilder: FormBuilder,
+              @Inject(MAT_DIALOG_DATA) public data: Client) {
     dialogRef.disableClose = true;
   }
 
   ngOnInit(): void {
+    if (this.data) {
+      const selectedClient = this.data;
+      const formPhoneNumber = new PhoneNumber(
+        this.data.phoneNumber.substr(0, 3),
+        this.data.phoneNumber.substr(3, 3),
+        this.data.phoneNumber.substr(6, 4)
+      );
+      this.clientForm.patchValue({
+        firstName: selectedClient.firstName,
+        lastName: selectedClient.lastName,
+        address: selectedClient.address,
+        phoneNumber: formPhoneNumber,
+        email: selectedClient.email,
+        dateOfBirth: selectedClient.dateOfBirth,
+        preferredBrands: selectedClient.brandPreference,
+        preferredColors: selectedClient.colorPreference,
+      });
+    }
   }
 
   onCancel(): void {
@@ -38,6 +57,7 @@ export class ClientFormComponent implements OnInit {
       const phoneNumberObject = this.clientForm.controls.phoneNumber.value as PhoneNumber;
       const clientFormObject = this.clientForm.value as Client;
       clientFormObject.phoneNumber = phoneNumberObject.area + phoneNumberObject.exchange + phoneNumberObject.subscriber;
+      clientFormObject.id = this.data?.id;
       this.save.emit(clientFormObject);
       this.dialogRef.close();
     }
