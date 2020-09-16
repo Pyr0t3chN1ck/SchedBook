176a58a8314645e2df5e35f5dc71f3bd27a0d5de
Nick <nletdara@gmail.com>
2020-09-16 14:09:48 -0400

    created delete client UI

--- sched-book/src/app/modules/address-book/client-search/client-search.component.ts
+++ sched-book/src/app/modules/address-book/client-search/client-search.component.ts
@@ -4,6 +4,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
 import { ClientFormComponent } from '../client-form/client-form.component';
 import { Client } from 'src/app/shared/models';
+import { DeleteClientDialogComponent } from '../delete-client-dialog/delete-client-dialog.component';
 
 @Component({
   selector: 'app-client-search',
@@ -13,20 +14,34 @@ import { Client } from 'src/app/shared/models';
 
 export class ClientSearchComponent implements OnInit {
   dataSource = new MatTableDataSource<Client>([
-    { id: '1', firstName: 'John', lastName: 'Smith', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '2', firstName: 'Jane', lastName: 'Doe', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '3', firstName: 'Orlando', lastName: 'Bloom', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '4', firstName: 'Chris', lastName: 'Evans', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '5', firstName: 'Clark', lastName: 'Kent', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '6', firstName: 'Peter', lastName: 'Parker', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
-    { id: '7', firstName: 'Bruce', lastName: 'Wayne', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
-    email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: '' },
+    {
+      id: '1', firstName: 'John', lastName: 'Smith', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '2', firstName: 'Jane', lastName: 'Doe', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '3', firstName: 'Orlando', lastName: 'Bloom', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '4', firstName: 'Chris', lastName: 'Evans', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '5', firstName: 'Clark', lastName: 'Kent', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '6', firstName: 'Peter', lastName: 'Parker', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
+    {
+      id: '7', firstName: 'Bruce', lastName: 'Wayne', address: '123 City Ave, Cleveland, OH 44221', phoneNumber: '1234567890',
+      email: 'testmail@email.com', dateOfBirth: new Date(), brandPreference: '', colorPreference: '', notes: ''
+    },
   ]);
   @ViewChild(MatPaginator, { static: true }) searchResultTablePaginator: MatPaginator;
   tableColumns = ['name', 'address', 'phoneNumber', 'email', 'dob', 'actions'];
@@ -38,23 +53,37 @@ export class ClientSearchComponent implements OnInit {
     this.dataSource.paginator = this.searchResultTablePaginator;
   }
 
-  search() {
+  search(): void {
     this.dataSource.filter = (this.searchText) ?
       this.searchText.toLocaleLowerCase() : '';
   }
 
-  openAddDialog() {
+  openAddDialog(): void {
     const addClientDialogRef = this.dialog.open(ClientFormComponent);
     addClientDialogRef.componentInstance.save.subscribe(data => {
       this.addClient(data);
     });
   }
 
-  addClient(newClient: Client) {
+  addClient(newClient: Client): void {
     const data = this.dataSource.data;
     data.push(newClient);
     this.dataSource.data = data;
     console.log('Added New Client!');
+  }
+
+  openDeleteDialog(row: Client): void {
+    const deleteClientDialogRef = this.dialog.open(DeleteClientDialogComponent, {
+      data: row
+    });
+    deleteClientDialogRef.componentInstance.delete.subscribe(data => {
+      this.deleteClient(data);
+    });
+  }
+
+  deleteClient(deletedClient: Client): void {
+    this.dataSource.data = this.dataSource.data.filter(row => row.id !== deletedClient.id);
+    console.log('Deleted Client!');
   }
 
 }
--- sched-book/src/app/modules/address-book/delete-client-dialog/delete-client-dialog.component.ts
+++ sched-book/src/app/modules/address-book/delete-client-dialog/delete-client-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EventEmitter } from '@angular/core';
+import { Client } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-delete-client-dialog',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-client-dialog.component.css']
 })
 export class DeleteClientDialogComponent implements OnInit {
+  delete = new EventEmitter<Client>();
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<DeleteClientDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: Client) { }
 
   ngOnInit(): void {
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onDelete(): void {
+    this.delete.emit(this.data);
+    this.dialogRef.close();
   }
 
 }
