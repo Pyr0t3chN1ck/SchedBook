6067e048128dfe7618b17c79ecc73b5750e4c94c
Nick <nletdara@gmail.com>
2020-11-13 16:48:13 -0500

    created loadEmployees effect that reads from firebase

--- sched-book/src/app/app.component.ts
+++ sched-book/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadEmployees } from './state/actions/employees.actions';
+import { AppState } from './state/reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sidenavOpen = false;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadEmployees());
+  }
 }
--- sched-book/src/app/state/effects/employees.effects.ts
+++ sched-book/src/app/state/effects/employees.effects.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
+import * as employeeActions from '../actions/employees.actions';
+import { EmployeeEntity } from '../reducers/employees.reducer';
 
 
 @Injectable()
 export class EmployeesEffects {
 
+  constructor(private actions$: Actions, private service: EmployeeService) { }
 
-
-  constructor(private actions$: Actions) {}
-
+  loadEmployees$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(employeeActions.loadEmployees),
+      switchMap(() => this.service.getEmployees().pipe(
+        map(results => employeeActions.loadEmployeesSucceess({
+          payload: results.map(emp => {
+            return {
+              id: emp.payload.doc.id,
+              ...emp.payload.doc.data() as any
+            } as EmployeeEntity;
+          })
+        })),
+        catchError(results => of(employeeActions.loadEmployeesFail({ message: 'There was an issue loading employees.' })))
+      )
+      )
+    )
+  );
 }
